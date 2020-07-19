90ce0e74151a71ad8a0f47a66d8353cda303c16d
asifahmed786 <asifahmed1us1@gmail.com>
2020-07-19 12:04:45 +0500

    cosmetic changes in 52 to 57

--- Chapter 52-57/app.js
+++ Chapter 52-57/app.js
@@ -1,7 +1,7 @@
-var pathList = ["images/1.jpg","images/2.png","images/3.jpg","images/4.jpg"]
+var pathList = ["images/1.jpg", "images/2.png", "images/3.jpg", "images/4.jpg"]
 var div = document.getElementById("maindiv")
-for(var i = 0;i<pathList.length;i++){
-    div.innerHTML += "<img src="+pathList[i]+" onclick=callModal("+i+") width=300px>";
+for (var i = 0; i < pathList.length; i++) {
+  div.innerHTML += "<img src=" + pathList[i] + " onclick=callModal(" + i + ") width=300px>";
 }
 
 
@@ -21,16 +21,16 @@ var span = document.getElementsByClassName("close")[0];
 function callModal(imgPathIndex) {
   modal.style.display = "block";
   var imgplace = document.getElementById("imgplace")
-  imgplace.innerHTML = "<img src="+pathList[imgPathIndex]+" width=80%>";
+  imgplace.innerHTML = "<img src=" + pathList[imgPathIndex] + " width=80%>";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
@@ -39,11 +39,23 @@ window.onclick = function(event) {
 
 
 var para1 = document.getElementById("para1")
-
-function zoomIn(){
-alert(para1.style.fontSize = "20px")
+var fontSize = "16px"
+function zoomIn() {
+  var size2 = parseInt(fontSize.substr(0, fontSize.indexOf("px")));
+  size2 += 10;
+  fontSize = String(size2 + "px")
+  para1.style.fontSize = fontSize;
 }
 
-function zoomOut(){
-    alert(para1.style.fontSize = "16px")
+function zoomOut() {
+  var size2 = parseInt(fontSize.substr(0, fontSize.indexOf("px")));
+  if(size2>16){
+    size2 -= 10;
+  }
+  else{
+
+  }
+  
+  fontSize = String(size2 + "px")
+  para1.style.fontSize = fontSize
 }
